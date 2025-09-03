d5bc4fd051e02e96e4aafdb79404ca1306a5f32f
Saurabh <nucleusbentok@gmail.com>
2025-09-03 23:20:41 +0530

    Update authUtils.js

--- backend/src/utils/authUtils.js
+++ backend/src/utils/authUtils.js
@@ -5,7 +5,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const createToken = function(payload) {
-    return jwt.sign(payload, JWT_SECRET, {expiresIn: "7d"})
+    return jwt.sign(payload, process.env.JWT_SECRET, {expiresIn: "7d"})
 };
 
 export const verifyToken = async function(token) {
